cea346a56ec20a74fc398550c23a783d6ef5fd92
issamiso <101994309+issamiso@users.noreply.github.com>
2022-11-13 09:23:56 +0100

    Add files via upload

---
+++ setupmetasploit.sh
@@ -0,0 +1,7 @@
+termux-setup-storage
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install ruby -y
+wget https://raw.githubusercontent.com/issamiso/metasploit_termux/main/.setup_metasploit.py
+python3 .setup_metasploit.py
